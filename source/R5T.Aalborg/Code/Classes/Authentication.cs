44a376a6d080543724c889e9f868d9d2220809f8
MinexAutomation <minex.automation@gmail.com>
2020-07-20 23:08:41 -0700

    Added Authentication class.

---
+++ source/R5T.Aalborg/Code/Classes/Authentication.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace R5T.Aalborg
+{
+    public class Authentication
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
