4aafe144177d216abff36a7dd7c9276b1889776a
David Coats <david@getsafetycone.com>
2021-04-30 14:19:39 -0700

    Add R5T.Aalborg.Abstractions abstraction types project.

---
+++ source/R5T.Aalborg.Abstractions/Code/Interfaces/IAuthentication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace R5T.Aalborg
+{
+    /// <summary>
+    /// A simple username and password authentication representation.
+    /// </summary>
+    /// <remarks>
+    /// Interface is read-only.
+    /// </remarks>
+    public interface IAuthentication
+    {
+        string Username { get; }
+        string Password { get; }
+    }
+}
