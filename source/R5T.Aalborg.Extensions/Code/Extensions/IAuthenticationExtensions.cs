576b77ae1d9878e3a728592fdba72ec56d3fc394
David Coats <david@getsafetycone.com>
2021-04-30 15:30:30 -0700

    Add R5T.Aalborg.Extensions project.

---
+++ source/R5T.Aalborg.Extensions/Code/Extensions/IAuthenticationExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+
+
+namespace R5T.Aalborg.Extensions
+{
+    public static class IAuthenticationExtensions
+    {
+        public static NetworkCredential GetNetworkCredential(this IAuthentication authentication)
+        {
+            var networkCredential = new NetworkCredential(authentication.Username, authentication.Password);
+            return networkCredential;
+        }
+    }
+}
